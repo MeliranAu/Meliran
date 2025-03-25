6af04946f4fe75c0757c504bf016c674f97f5f7f
Behrang <BehrangBina@hotmail.com>
2025-03-25 23:30:56 +1100

    fix seeding

--- routes/events.js
+++ routes/events.js
@@ -1,21 +1,40 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../db');
 
 router.get('/', (req, res) => {
-  // Later: Fetch events from DB
-  const events = [{ id: 1, title: 'Sample Event', date: '2025-04-01' }];
-  res.render('events', { events });
+  db.all('SELECT * FROM events', [], (err, events) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send('Error fetching events');
+    }
+    res.render('events', { events });
+  });
 });
 
 router.get('/:id', (req, res) => {
-  // Later: Fetch event by ID
-  const event = { id: req.params.id, title: 'Sample Event', rsvpCount: 5 };
-  res.render('event-details', { event });
+  const id = req.params.id;
+  db.get('SELECT * FROM events WHERE id = ?', [id], (err, event) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send('Error fetching event');
+    }
+    if (!event) {
+      return res.status(404).send('Event not found');
+    }
+    res.render('event-details', { event });
+  });
 });
 
 router.post('/:id/rsvp', (req, res) => {
-  // Later: Update RSVP in DB
-  res.redirect(`/events/${req.params.id}`);
+  const id = req.params.id;
+  db.run('UPDATE events SET rsvpCount = rsvpCount + 1 WHERE id = ?', [id], (err) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send('Error updating RSVP');
+    }
+    res.redirect(`/events/${id}`);
+  });
 });
 
 module.exports = router;
